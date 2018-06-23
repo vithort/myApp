d6e37b1d3f01274928baa48e6bce34888a83c327
Vithor Carvalho <vithort@gmail.com>
2018-06-23 11:35:54 -0300

    Seção 6 Aula 19 Navegando da página Intro para a página Principal
    .. cria a função para ir para a Home

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the IntroPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { TabsPage } from '../tabs/tabs';
 
 @IonicPage()
 @Component({
-  selector: 'page-intro',
-  templateUrl: 'intro.html',
+  selector: 'page-intro'
+  ,templateUrl: 'intro.html'
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController
+    ,public navParams: NavParams
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
+  }
+
+  goToTabsPage() {
+    this.navCtrl.push(TabsPage);
   }
 
 }
