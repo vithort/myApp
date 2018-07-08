d776e16f47002ba119cb6714d546f6ecd973cf2c
Vithor Carvalho <vithort@gmail.com>
2018-07-08 12:43:04 -0300

    Seção 12 Aula 40 Fazendo o slider abrir uma vez só
    . verificar no LocalStorage se precisa abrir ou não a página...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-//import { TabsPage } from '../pages/tabs/tabs';
+import { TabsPage } from '../pages/tabs/tabs';
 import { IntroPage } from '../pages/intro/intro';
 import { ConfigProvider } from '../providers/config/config';
 
@@ -15,7 +15,7 @@ import { ConfigProvider } from '../providers/config/config';
 })
 export class MyApp {
   //rootPage:any = TabsPage;
-  rootPage:any = IntroPage;
+  rootPage:any;
 
   constructor(
     platform: Platform
@@ -26,6 +26,12 @@ export class MyApp {
     platform.ready().then(() => {
 
       let config = configProvider.getConfigData();
+      if (config == null) {
+        this.rootPage = IntroPage;
+        configProvider.setConfigData(false);
+      } else {
+        this.rootPage = TabsPage;
+      }
       console.log(config);
 
       statusBar.styleDefault();
