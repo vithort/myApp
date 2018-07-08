8f27cdee792c51ead87c48471fb41c2a0f1bf5cd
Vithor Carvalho <vithort@gmail.com>
2018-07-08 16:01:07 -0300

    Seção 15 Aula 48 Usando o plugin nativo da Câmera
    . instalar plugin nativo da camera com os comandos:
    > ionic cordova plugin add cordova-plugin-camera
    > npm install --save @ionic-native/camera
    . fazer o import na pagina que será utilizada
    . adicionar Camera no construtor e providers
    . importar funcao de Camera nativa Ionic
    . para rodar no browser, executar o comando:
    > ionic cordova platform add browser
    . executar o app como:
    > ionic cordova run browser
    ... pronto

--- src/pages/atletas/atletas.ts
+++ src/pages/atletas/atletas.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartolaProvider } from '../../providers/cartola/cartola';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -8,14 +9,18 @@ import { CartolaProvider } from '../../providers/cartola/cartola';
   ,templateUrl: 'atletas.html'
   ,providers: [
     CartolaProvider
+    ,Camera
   ]
 })
 export class AtletasPage {
+
+  img = "";
 
   constructor(
     public navCtrl: NavController
     ,public navParams: NavParams
     ,private _cartolaProvider: CartolaProvider
+    ,private camera: Camera
   ) {
   }
 
@@ -24,6 +29,23 @@ export class AtletasPage {
     this._cartolaProvider.atletas().subscribe(data => {
         console.log(data);
       })
+  }
+
+  tirarFoto() {
+    const options: CameraOptions = {
+      quality: 100
+      ,destinationType: this.camera.DestinationType.DATA_URL
+      ,encodingType: this.camera.EncodingType.JPEG
+      ,mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
