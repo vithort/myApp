dc061a59be35a66a04b930fe49436b36dd801b5e
Vithor Carvalho <vithort@gmail.com>
2018-07-08 13:17:10 -0300

    Seção 14 Aula 43 Exibir mensagem enquanto a lista de filmes é carregada
    . adicionar no construtor a dependencia: public loadingCtrl no feed.ts: LoadingController
    . criar funçao para abrir e fechar o Loader no feed.ts: abrirCarregando e fecharCarregando
    . alterar no feed.ts o ionViewDidLoad por ionViewDidEnter: o DidLoad só carrega uma vez, enquanto o DidEnter carrega toda vez que acessa a página

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
 
 @IonicPage()
@@ -24,12 +24,26 @@ export class FeedPage {
   }
 
   public lista_filmes = new Array<any>();
+  public loader;
 
   constructor(
     public navCtrl: NavController
     ,public navParams: NavParams
     ,private movieProvider: MovieProvider
+    ,public loadingCtrl: LoadingController
   ) {
+  }
+
+  abrirCarregando() {
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes...",
+      //duration: 3000
+    });
+    this.loader.present();
+  }
+
+  fecharCarregando() {
+    this.loader.dismiss();
   }
 
   /*public somaDoisNumeros(num1: number, num2: number): void {
@@ -43,18 +57,21 @@ export class FeedPage {
   }*/
 
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter FeedPage');
     //this.somaDoisNumeros();
     //this.somaDoisNumeros(5, 3);
+    this.abrirCarregando();
     this.movieProvider.getLastestMovies().subscribe(
       data => {
         const response = (data as any);
         const objeto_retorno = JSON.parse(response._body);
         this.lista_filmes = objeto_retorno.results;
         console.log(objeto_retorno);
+        this.fecharCarregando();
     }, error => {
       console.log(error);
+      this.fecharCarregando();
     })
   }
 
